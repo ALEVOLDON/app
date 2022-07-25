de76ad7bd2312592defe61dab81dc82e3bd86512
Vladimir Rybalsky <alevoldon@gmail.com>
2022-07-25 20:26:47 +0300

    upd app

--- sketch.js
+++ sketch.js
@@ -1,12 +1,22 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
+let y = 100;
 
+// The statements in the setup() function
+// execute once when the program begins
+function setup() {
+  // createCanvas must be the first statement
+  createCanvas(windowWidth, windowHeight);
+  stroke(255); // Set line drawing color to white
+  frameRate(30);
+}
+// The statements in draw() are executed until the
+// program is stopped. Each statement is executed in
+// sequence and after the last line is read, the first
+// line is executed again.
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+  background(0); // Set the background to black
+  y = y - 1;
+  if (y < 0) {
+    y = height;
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  line(0, y, width, y);
 }
